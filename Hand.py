83674b9cb775ef4256479033d917467a60b9e29b
Fernando SPADARO <rfspadaro@gmail.com>
2016-04-22 15:58:48 -0700

    Add Hand and HandTest -initial commit-

---
+++ Hand.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import Card
+
+class Hand:
+  """ A Hand of Poker cards """
+
+  VAL_NONE = -1
+  VAL_ROYAL_FLUSH = 0
+  
+  def __init__(self):
+    self.hand = []
+
+  def accept(self, card):
+    self.hand.append(card)
+
+  def evaluate(self):
+    return self.nvalToNumericRank[self.numericValue]
+
+  def symbolicRank(self):
+    n = self.numericRank()
+    return self.numericRankToSymbolicRank[n]
+
+  def numericRankToStringRank(self, n):
+    return self.numericRankToSymbolicRank[n]
+
+if __name__ == '__main__':
+  c = Card(14)
+  print c.suit()
+
