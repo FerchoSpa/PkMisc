b966e01eb827b767fdcb40f960e375d97490be53
Fernando SPADARO <rfspadaro@gmail.com>
2016-10-15 22:20:11 -0700

    Finished conversion of Card

--- Card.cpp
+++ Card.cpp
@@ -2,6 +2,12 @@
 
 #include <stdio.h>
 
-Card::Card() {
+Card::Card(int numericValue) {
 	printf("Creating card\n");
+    this->numericValue = numericValue;
+    int n = numericValue % 13;
+    this->numericRank = n+1;
+    this->suit = nvalToSuit[numericValue];
+    this->symbolicRank = strSymbolicRanks[n+1];
+
 }
--- Card.h
+++ Card.h
@@ -2,8 +2,39 @@
 #define _CARD_H
 
 class Card {
+	enum Suits {
+		HEARTS   = 0,
+		DIAMONDS = 1,
+		SPADES   = 2,
+		CLUBS    = 3
+	};
+
+	// Short visual representation of Suits:
+	//  - Heart, Diamond, Spade and Clubs
+	char strSuits[4] = {'H', 'D', 'S', 'C'};
+
+	// Numeric value (0 - 51) to Suits(Heart, Diamond, Spade, Club)
+	int nvalToSuit[] = {HEARTS, HEARTS, HEARTS, HEARTS, HEARTS, HEARTS, \
+	                HEARTS, HEARTS, HEARTS, HEARTS, HEARTS, HEARTS, HEARTS, \
+	                DIAMONDS, DIAMONDS, DIAMONDS, DIAMONDS, DIAMONDS, DIAMONDS, \
+	                DIAMONDS, DIAMONDS, DIAMONDS, DIAMONDS, DIAMONDS, DIAMONDS, DIAMONDS, \
+	                SPADES, SPADES, SPADES, SPADES, SPADES, SPADES, \
+	                SPADES, SPADES, SPADES, SPADES, SPADES, SPADES, SPADES, \
+	                CLUBS, CLUBS, CLUBS, CLUBS, CLUBS, CLUBS, \
+	                CLUBS, CLUBS, CLUBS, CLUBS, CLUBS, CLUBS, CLUBS};
+
+	int strSymbolicRanks = {'Nada', '0', 'A', '2', '3', '4', '5', '6', '7',
+			'8', '9', 'T', 'J', 'Q', 'K'
+	};
+
+	int numericValue;
+	int numericRank;
+	int suit;
+	char symbolicRank;
+
+
 public:
-	Card();
+	Card(int numericValue);
 };
 
 #endif
